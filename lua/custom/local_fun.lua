f4642adc54ea7dd90f7754f38100b6f67a8d7e38
cajone <peat@blueyonder.co.uk>
2023-08-12 13:29:42 +0100

    Change the curent working directory

--- lua/custom/local_fun.lua
+++ lua/custom/local_fun.lua
@@ -16,3 +16,23 @@ function ToggleVimwikiConceallevel()
     return
   end
 end
+
+
+-- Change the LCD to the current git buffer root
+function ChangeWorkingDirectoryToGitRoot()
+    local bufnr = vim.fn.bufnr('%')
+    local buffer_directory = vim.fn.expand('%:p:h')
+
+    -- Run git rev-parse to find the root of the repository
+    local git_root = vim.fn.systemlist('git -C ' .. buffer_directory .. ' rev-parse --show-toplevel')[1]
+
+    if git_root then
+        vim.fn.execute("lcd " .. git_root)
+    else
+        print("Not in a Git repository")
+    end
+end
+
+-- Example usage
+vim.api.nvim_set_keymap('n', '<Leader>lcd', [[<Cmd>lua ChangeWorkingDirectoryToGitRoot()<CR>]], { noremap = true, silent = true })
+
