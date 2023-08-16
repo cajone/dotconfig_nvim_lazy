e66c5140462092e4ec854b3362eca16541646db6
cajone <peat@blueyonder.co.uk>
2023-08-17 00:01:13 +0100

    Configuration update

--- lua/plugins/lazy_load.lua
+++ lua/plugins/lazy_load.lua
@@ -40,7 +40,7 @@ require("lazy").setup({
 }) -- End of Lazy Plugin install section 
 
 
-  function set_terminal_keymaps()
+  function Set_terminal_keymaps()
     local opts = {noremap = true}
     vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
     vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts)
@@ -50,7 +50,7 @@ require("lazy").setup({
     vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
   end
 
-  vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+  vim.cmd('autocmd! TermOpen term://* lua Set_terminal_keymaps()')
 --  local Terminal = require("toggleterm.terminal").Terminal
 --  local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
 
