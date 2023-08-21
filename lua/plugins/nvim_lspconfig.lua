2601de049d8b1111e2c503a92523e2f08721e547
cajone <peat@blueyonder.co.uk>
2023-08-21 02:26:24 +0100

    Removed linting from lua LSP, issue with vim undef

--- lua/plugins/nvim_lspconfig.lua
+++ lua/plugins/nvim_lspconfig.lua
@@ -14,11 +14,11 @@ local M = {
     lspconfig.lua_ls.setup { -- Will parse lua files
       cmd = { "lua-language-server" },
       settings = {
-        Lua = {                  -- this really should be set to 'lua' but is causing an issue with vim global
+        Lua = { -- this really should be set to 'lua' but is causing an issue with vim global
           cmd = { "luacheck" },
           diagnostics = {
             enable = true,
-            globals = {'vim'},
+            globals = { 'vim' },
           },
         },
         languages = {
