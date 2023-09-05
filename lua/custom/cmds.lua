8cf1cc292cbef40cef174c766f6af6d22acb6d73
cajone <peat@blueyonder.co.uk>
2023-09-05 12:40:18 +0100

    Update lsp_config branch

--- lua/custom/cmds.lua
+++ lua/custom/cmds.lua
@@ -1,15 +1,21 @@
 -- Set syntax highlighting for Jenkins files with no extension
-vim.api.nvim_exec([[
+vim.api.nvim_exec(
+  [[
   autocmd BufNewFile,BufRead JenkinsFile,*/Jenkinsfile,Jenkinfile* set filetype=groovy
-]], false)
+]],
+  false
+)
 
 -- Auto run rubocop vore ruby files
-vim.api.nvim_exec([[
+vim.api.nvim_exec(
+  [[
   autocmd BufWritePost *.rb :silent !rubocop --auto-correct %
-]], false)
+]],
+  false
+)
 
 -- Change the LCD path for Vimwiki
 vim.cmd([[autocmd BufEnter *.wiki lcd ~/vimwiki]])
 
 -- Open Terminal
-vim.cmd('autocmd! TermOpen term://* lua Set_terminal_keymaps()')
+vim.cmd("autocmd! TermOpen term://* lua Set_terminal_keymaps()")
