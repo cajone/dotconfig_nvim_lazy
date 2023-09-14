975f734b2fc09562f422dcd557877a214e027a59
cajone <peat@blueyonder.co.uk>
2023-09-14 01:04:34 +0100

    Refactored statusline

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -32,25 +32,22 @@ return {
             symbols = { error = "E", warn = "W", info = "I", hint = "H" },
           },
         },
-        lualine_v = {
+        lualine_x = {
           {
             lazy_status.updates,
             cond = lazy_status.has_updates,
             color = { fg = "#ff9e64" },
           },
         },
-        lualine_w = {
+        lualine_y = {
+          "progress",
+          "|",
           "filetype",
         },
-        lualine_x = {
-          "progress",
-        },
-        lualine_y = {     -- Show LCD
+        lualine_z = {
           function()
             return vim.fn.getcwd()
           end,
-        },
-        lualine_z = {
           function()
             return os.date("%H:%M")
           end,
