31e779894479cd51e5901e31728e4a3e465ed809
cajone <peat@blueyonder.co.uk>
2023-10-02 19:43:57 +0100

    Refactored working languages

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,7 +1,13 @@
 M = {
   configs = require('nvim-treesitter.configs').setup {
     require('nvim-treesitter.configs').setup{
-      ensure_installed = { 'bash', 'groovy', 'python', 'ruby', 'sql', 'typescript' },
+      ensure_installed = {
+        'bash',
+        'groovy',
+        'lua',
+        'python',
+        'ruby',
+      },
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = true,
