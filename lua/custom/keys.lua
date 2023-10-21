f75166aa760a8877b77d0f83f1c4ca9e6b2dc055
cajone <peat@blueyonder.co.uk>
2023-10-21 12:10:21 +0100

    Refactored

--- lua/custom/keys.lua
+++ lua/custom/keys.lua
@@ -56,19 +56,18 @@
   map("n", "<Leader>NL", ":lua vim.lsp.buf.linting()<CR>", { noremap = true })
 
 -- <F> Keys
-  -- Toggle Vimwiki URL's
-    map("n", "<F8>", ":lua ToggleVimwikiConceallevel()<CR>", { noremap = true })
   -- resize current focused vertical panel by 5 chars
     map("n", "<F3>", ":vertical resize -5<CR>", { noremap = true })
     map("n", "<F4>", ":vertical resize +5<CR>", { noremap = true })
   -- Spelling 
-  -- F5 will find next word in doc, 
+    -- F5 will find next word in doc, 
     map("n", "<F5>", "]s", { noremap = true })
-  -- F6 will add word to local dictionary
+    -- F6 will add word to local dictionary
     map("n", "<F6>", "zg", { noremap = true })
-  -- F7 will prompt to select from a dictionary
+    -- F7 will prompt to select from a dictionary
     map("n", "<F7>", "z=", { noremap = true })
-
+  -- Toggle Vimwiki URL's
+    map("n", "<F8>", ":lua ToggleVimwikiConceallevel()<CR>", { noremap = true })
 
 -- Mapping for cookstyle
   map("n", "<leader>cs", ":!cookstyle -a %<CR>", { noremap = true })
