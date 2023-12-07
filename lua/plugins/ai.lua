c763b1d47a4ff3c5032c362c242df21dd01d2c32
cajone <peat@blueyonder.co.uk>
2023-12-07 00:01:16 +0000

    Configuration update

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -3,13 +3,8 @@ return {
     "David-Kunz/gen.nvim",
     config = function()
       require('gen').prompts['Devops'] = {
-        model = 'orca2',
-        prompt = [[You are a senior DevOps engineer, answering questions about Lua, nvim, Jenkins, Ansible, Chef, Ruby, Docker, Docker-Compose and Python. Provide code examples where appropriate. $input\n$text]],
-        replace = true
-      }
-      require('gen').prompts['Ansible'] = {
-        model = 'deepseek-coder:6.7b',
-        prompt = [[You are a senior DevOps engineer, answering questions about Ansible. Provide code examples where appropriate. $input\n$text]],
+        model = 'wizardcoder',
+        prompt = [[You are a senior DevOps engineer, answering questions about  Ansible, Docker, Docker-Compose and Python. Provide code examples where appropriate. $input\n$text]],
         replace = true
       }
       require('gen').prompts['Lua'] = {
@@ -18,8 +13,8 @@ return {
         replace = true
       }
       require('gen').prompts['General'] = {
-        model = 'llama2',
-        prompt = [[You are a senior DevOps engineer, answering questions about nvim, Jenkins, Ansible, Chef, Ruby, Docker, Docker-Compose and Python. $input\n$text]],
+        model = 'mistral',
+        prompt = [[You are a senior AI engineer, answering questions about LLMs, AI frameworks. $input\n$text]],
         replace = true
       }
     end
