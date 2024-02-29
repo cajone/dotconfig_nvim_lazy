506754c015dbfb5d5b1848cf342dc708e7569876
212560709 <peter.thomsop@ge.com>
2024-02-29 20:33:05 +0000

    MOdified dadbod-ui plugin

--- lua/plugins/dadbod-ui.lua
+++ lua/plugins/dadbod-ui.lua
@@ -5,6 +5,12 @@ return {
   { "kristijanhusak/vim-dadbod-completion", },    -- DB autocompletion
   config = function()
     local db_ui_icons = require("db_ui_icons")
+    local db_ui_notification = require("db_ui_notification") 
+    db_ui_notification.setup({
+      options = {
+        db_ui_notification_width = 1000,
+      },
+    })
     db_ui_icons.setup({
       options = {
        expanded = {
