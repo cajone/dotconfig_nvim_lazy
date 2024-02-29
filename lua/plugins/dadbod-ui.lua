8929f85025c81a144c887ce5da67c0e18bdfdf3d
cajone <peat@blueyonder.co.uk>
2024-02-29 19:18:06 +0000

    Fixed dadbod ui plugin

--- lua/plugins/dadbod-ui.lua
+++ lua/plugins/dadbod-ui.lua
@@ -3,29 +3,29 @@
 return {
   { "kristijanhusak/vim-dadbod-ui", },
   { "kristijanhusak/vim-dadbod-completion", },    -- DB autocompletion
---  config = function()
---    local du_ui_icons = require("db_ui_icons")
---    db_ui_icons.setup({
---      options = {
---       'expanded'= {
---               'db'= '-',
---               'buffers'= '-',
---               'saved_queries'= '-',
---               'schemas'= '-',
---               'schema'= '-',
---               'tables'= '-',
---               'table'= '-',
---       },
---       'collapsed'= {
---               'db'= '+',
---               'buffers'= '+',
---               'saved_queries'= '+',
---               'schemas'= '+',
---               'schema'= '+',
---               'tables'= '+',
---               'table'= '+',
---         },
---       },
---    })
---  end,
+  config = function()
+    local db_ui_icons = require("db_ui_icons")
+    db_ui_icons.setup({
+      options = {
+       expanded = {
+               db= '-',
+               buffers= '-',
+               saved_queries= '-',
+               schemas= '-',
+               schema= '-',
+               tables= '-',
+               table= '-',
+       },
+       collapsed= {
+               db= '+',
+               buffers= '+',
+               saved_queries= '+',
+               schemas= '+',
+               schema= '+',
+               tables= '+',
+               table= '+',
+         },
+       },
+    })
+  end,
 }
