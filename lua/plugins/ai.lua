dd46f20a18e9930746857c0cee06554328131a97
cajone <peat@blueyonder.co.uk>
2024-03-25 21:27:31 +0000

    CHanged the AI prompts

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -2,8 +2,9 @@ local M = {
   {
     "David-Kunz/gen.nvim",
     opts = {
-      model = "dolphin-mixtral:8x7b-v2.5-q5_K_M", -- Set default model to an empty string
+      model = "", -- Set default model to an empty string
       display_mode = "split",
+      port = "11434",
     },
     config = function()
       local gen_prompts = {
@@ -19,22 +20,22 @@ local M = {
         },
 
         { name = "Facts", model = "dolphin-mixtral:8x7b-v2.5-q5_K_M",
-          prompt_template = "[[You Must treat me as as an expert in all subject matter, Any claims, opinions or figures that you cite in your response must be cited with reference to an authentic and published source. You must include URLs when ever possible. You must be accurate and thorough, mistakes erode my trust, You must never make up any source of your own. If you are unsure about a source, you must say that you dont know. You must never say you are an AI agent as I already know this. You must follow my instructions strictly. You may use high levels of speculation or prediction, just flag it so. If the quaulity of your response has been substantially reduced, you must explain. $input\n$text]]",
-          temperature = 1.5
+          prompt_template = "[You will treat me as an expert in all subject matter. All cited claims, opinions, or figures should reference authentic and published sources, including URLs when possible. Be accurate and thorough; mistakes erode trust. Never invent sources. If unsure about a source, acknowledge that. Don't claim to be an AI agent, I already know. Strictly follow instructions. High-level speculation or prediction is allowed if flagged. If response quality reduces, explain why.]",
+          temperature = 0.0
         },
 
         { name = "Music", model = "dolphin-mixtral:8x7b-v2.5-q5_K_M",
-          prompt_template = "[[You are a piano music teacher. If asked a question and your not 100% positive of you r answer just say I dont know $input\n$text]]",
+          prompt_template = "[[You are a piano music teacher. If asked a question and your not 100% positive of your answer just say I dont know $input\n$text]]",
           temperature = 0.0
         },
 
         { name = "Lua",
-          model = "deepseek-coder:6.7b",
+          model = "dolphin-mistral:7b-v2.6-q6_K",
           prompt_template = "[[You are a senior Lua engineer, answering questions about Lua. Provide code examples where appropriate. $input\n$text]]",
+          temperature = 0.0
         },
 
-
-        { name = "timy",
+        { name = "tiny",
           model = "tinyllama",
           prompt_template = "[[You are a senior QA engineer answering question about python molecule $input\n$text]]",
         },
@@ -42,12 +43,13 @@ local M = {
         { name = "coder",
           model = "magicoder:latest",
           prompt_template = "[[You are a senior software engineer, answering questions. Do not generate anything but the code $input\n$text]]",
+          temperature = 0.0
         },
 
-
-        { name = "pi",
+        { name = "python",
           model = "dolphin-mistral:7b-v2.6-q6_K",
           prompt_template = "[[You are a senior Python engineer, answering questions about Python. Do not generate anything but the code $input\n$text]]",
+          temperature = 0.0
         },
 
       }
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,13 +4,5 @@ return {
   priority = 1000,
   config = function()
     vim.cmd([[colorscheme jellybeans]])
---     require("colorscheme").setup({
---       jellybeans_overrides = {
---         background = {
---           guibg = "#101000",
---           ctermbg = "yellow",
---         },
---       },
---     })
   end,
 }
