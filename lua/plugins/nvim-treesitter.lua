24b0b7a85c6802ffbfa756a153d6239f6aefadd3
cajone <peat@blueyonder.co.uk>
2024-04-04 00:01:11 +0100

    Configuration update

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,20 +1,30 @@
 return {
-  configs = require('nvim-treesitter.configs').setup {
-    build = ":TSUpdate",
-    event = { "BufReadPre", "BufNewFile" },
-    require('nvim-treesitter.configs').setup{
---      ensure_installed = { 'bash', 'groovy', 'json', 'python', 'ruby', 'sql', 'typescript' },
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = { "windwp/nvim-ts-autotag", },
+    config = function()
+    local treesitter = require('nvim-treesitter.configs')
+    treesitter.setup({
+      ensure_installed = { 'bash', 'groovy', 'json', 'python', 'ruby', 'sql', 'typescript' },
       sync_install = false,
       auto_install = true,
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
       },
-      increamental_selection = {
-        enable = false,
-      },
       indent = { enable = true, },
       query_linter = true,
-    },
-  }
+      autotag = { enable = true },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      }
+    })
+  end
 }
