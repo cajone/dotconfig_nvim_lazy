1e6e6100ce3dfd862004f8460ecef993318956b3
cajone <peat@blueyonder.co.uk>
2024-04-09 00:01:19 +0100

    Configuration update

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -6,12 +6,17 @@ return {
     size = 20,
     direction = "float",
     hide_numbers = true,
-    shading_factor = 2,
+    shading_factor = 4,
     shade_filetypes = {},
     start_in_insert = true,
     insert_mapping = true,
     persistent_size = true,
     close_on_exit = true,
     shell = vim.o.shell,
+    hightlights = {
+      Normal = {
+        guibg = "#AAA777",
+      }
+    }
   },
 }
