94d82c0b93eebbcdfaa752623f2287b15e84eab3
cajone <peat@blueyonder.co.uk>
2024-04-18 13:54:54 +0100

    Updated LLM models

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -6,48 +6,53 @@ M = {
       display_mode = "split",
       port = "11434",
     },
+    -- The models have a prefix A_ / M_ / H_ which refers to there size
+    -- L_ lightweight < 5GB
+    -- M_ Mediumweight > 5 < 15GB
+    -- H_ Heavyweight >15GB in size
     config = function()
       local gen_prompts = {
-        { name = "Agen", model = "dolphin-mistral:7b", prompt_template = "$input\n$text" },
+        { name = "A_Agen", model = "dolphin-mistral:7b", prompt_template = "$input\n$text" },
 
-        { name = "Amix", model = "dolphin-mistral:7b-v2.6-q6_K",
+        { name = "M_Amix", model = "dolphin-mistral:7b-v2.6-q6_K",
           prompt_template = "[[You are a senior AI engineer, answering questions about LLMs, AI frameworks. Focus should be on solutions that running LLM's locally $input\n$text]]"
         },
 
-        { name = "Bmix", model = "dolphin-mixtral:8x7b-v2.5-q5_K_M",
+        { name = "M_Bmix", model = "dolphin-mixtral:8x7b-v2.5-q5_K_M",
           prompt_template = "[[You are a story teller that focuses on detail and scene setting $input\n$text]]",
           temperature = 1.5
         },
 
-        { name = "Facts", model = "dolphin-mixtral:8x7b-v2.5-q5_K_M",
+        { name = "M_Facts", model = "dolphin-mixtral:8x7b-v2.5-q5_K_M",
           prompt_template = "[You will treat me as an expert in all subject matter. All cited claims, opinions, or figures should reference authentic and published sources, including URLs when possible. Be accurate and thorough; mistakes erode trust. Never invent sources. If unsure about a source, acknowledge that. Don't claim to be an AI agent, I already know. Strictly follow instructions. High-level speculation or prediction is allowed if flagged. If response quality reduces, explain why.]",
           temperature = 0.0
         },
 
-        { name = "Music", model = "dolphin-mixtral:8x7b-v2.5-q5_K_M",
+        { name = "M_Music", model = "dolphin-mixtral:8x7b-v2.5-q5_K_M",
           prompt_template = "[[You are a piano music teacher. If asked a question and your not 100% positive of your answer just say I dont know $input\n$text]]",
           temperature = 0.0
         },
 
-        { name = "Lua",
+        { name = "M_Lua",
           model = "dolphin-mistral:7b-v2.6-q6_K",
           prompt_template = "[[You are a senior Lua engineer, answering questions about Lua. Provide code examples where appropriate. $input\n$text]]",
           temperature = 0.0
         },
 
-        { name = "tiny",
-          model = "tinyllama",
-          prompt_template = "[[You are a senior QA engineer answering question about python molecule $input\n$text]]",
-        },
-
-        { name = "coder",
+        { name = "A_coder",
           model = "magicoder:latest",
           prompt_template = "[[You are a senior software engineer, answering questions. Do not generate anything but the code $input\n$text]]",
           temperature = 0.0
         },
 
-        { name = "python",
-          model = "dolphin-mistral:7b-v2.6-q6_K",
+        { name = "H_coder",
+          model = "deepseek-coder:33b",
+          prompt_template = "[[You are a senior software engineer, answering questions. Do not generate anything but the code $input\n$text]]",
+          temperature = 0.0
+        },
+
+        { name = "A_python",
+          model = "codellama:python",
           prompt_template = "[[You are a senior Python engineer, answering questions about Python. Do not generate anything but the code $input\n$text]]",
           temperature = 0.0
         },
