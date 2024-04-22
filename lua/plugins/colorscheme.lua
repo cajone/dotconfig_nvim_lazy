2b9b54f7232d6feeb3282d1b0bbd486b72d4dc9a
cajone <peat@blueyonder.co.uk>
2024-04-23 00:18:32 +0100

    hanged colour scheme ( again )

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,22 @@
 M = {
---  "rainglow/vim",
---  "rafi/awesome-vim-colorschemes",
-  "tonsky/neovim-colors.git",
+  "olivercederborg/poimandres.nvim",
   lazy = false,
   priority = 1000,
   config = function()
-    vim.cmd([[colorscheme gotham]])
+    require('poimandres').setup{}
   end,
+
+  init = function()
+    vim.cmd([[colorscheme poimandres]])
+  end
 } return M
+
+
+-- Other color schemes under consideration
+--  "rainglow/vim",
+--  "rafi/awesome-vim-colorschemes",
+--  "tonsky/neovim-colors.git",
+--  "jacoborus/tender.vim",
 
 -- gotham
 -- lunaperche
