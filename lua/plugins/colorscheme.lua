3bcaede3f126523fe48881cbb14287ea43e968f3
cajone <peat@blueyonder.co.uk>
2024-04-23 11:08:34 +0100

    Refactored

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,29 +1,21 @@
 M = {
--- Other color schemes under consideration
---  "rainglow/vim",
---  "rafi/awesome-vim-colorschemes",
---  "StarryLeo/starry-vim-colorschemes",
   "folke/tokyonight.nvim",
---  "jacoborus/tender.vim",
-
---  "olivercederborg/poimandres.nvim",
   lazy = false,
   priority = 1000,
   opts = {},
---  config = function()
---    require('jellybeans').setup{
---      bold_vert_split = true,
---      dim_nc_background = true,
---      disable_background = true,
---      disable_float_background = true,
---      disable_italics = true,
---    }
---  end,
 
   init = function()
     vim.cmd([[colorscheme tokyonight]])
   end,
 } return M
+
+
+-- Other color schemes under consideration
+--  "rainglow/vim",
+--  "rafi/awesome-vim-colorschemes",
+--  "StarryLeo/starry-vim-colorschemes",
+--  "jacoborus/tender.vim",
+--  "olivercederborg/poimandres.nvim",
 
 -- gotham
 -- lunaperche
