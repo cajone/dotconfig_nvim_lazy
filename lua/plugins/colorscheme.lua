aabd3813fff858e14a1325ff78b28594227e0deb
cajone <peat@blueyonder.co.uk>
2024-04-25 00:01:12 +0100

    Configuration update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,12 @@
 M = {
   "folke/tokyonight.nvim",
+--  "cajone/cajone-nvim",
   lazy = false,
   priority = 1000,
   opts = {},
 
   init = function()
-    vim.cmd([[colorscheme tokyonight-storm]])
+    vim.cmd([[colorscheme tokyonight]])
   end,
 } return M
 
