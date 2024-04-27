256d1c906350fd12b2dd877caabafb01a8e691ef
cajone <peat@blueyonder.co.uk>
2024-04-27 14:05:59 +0100

    Automatic formatting

--- init.lua
+++ init.lua
@@ -1,7 +1,6 @@
-
 -- Change the <LEADER> Key
-vim.g.mapleader = '\\'
-vim.g.maplocalleader = '\\'
+vim.g.mapleader = "\\"
+vim.g.maplocalleader = "\\"
 
 -- Start loading the contents of lua/init.lua
-require('init')
+require("init")
