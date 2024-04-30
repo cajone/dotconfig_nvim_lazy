09f76831970390ecdca0ba8008d52de2a6f5b467
cajone <peat@blueyonder.co.uk>
2024-04-30 13:12:13 +0100

    Removed reference to treesitter, added mason plugin

--- lua/plugins/lazy_load.lua
+++ lua/plugins/lazy_load.lua
@@ -43,7 +43,7 @@ require("lazy").setup({
 
   -- LSP / Auto completion language Plugins
 --  { import = "plugins.lsp.nvim-cmp" }, -- Auto Completion
-  --  { import = "plugins.lsp.mason" }, -- LSP installer
+  { import = "plugins.lsp.mason" }, -- LSP installer
 --  { import = "plugins.lsp.none-ls" }, -- null-ls replacement
   --  { import = "plugins.lsp.debug" }, -- LSP Debug
   --  { import = "plugins.lsp.lsp_config" }, -- LSP configuration
