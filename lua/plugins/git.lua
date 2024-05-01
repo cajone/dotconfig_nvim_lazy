1c33bc2f956b4587965ef8610994127177693ada
cajone <peat@blueyonder.co.uk>
2024-05-01 13:40:19 +0100

    Added Git

---
+++ lua/plugins/git.lua
@@ -0,0 +1,27 @@
+M = {
+  -- Fugitive
+  { "tpope/vim-fugitive" }, -- Fugitive ( Git)
+  { "tpope/vim-rhubarb" }, -- Git Support
+  { "cohama/agit.vim", },                                   -- Git history Tree
+
+  -- Git Diffview
+  { "sindrets/diffview.nvim" }, -- gitdiff view
+  { "nvim-lua/plenary.nvim" },
+
+  -- GitSigns
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      local gitsigns = require("gitsigns")
+      gitsigns.setup()
+    end,
+  },
+
+  -- Git Worktrees
+  "ThePrimeagen/git-worktree.nvim",
+  config = function()
+    require("git-worktree").setup()
+    require("telescope").load_extension("git_worktree")
+  end,
+}
+return M
