ee44f94cc3df010a0cf8ae096e492d022ee5fdd7
cajone <peat@blueyonder.co.uk>
2024-05-01 13:48:24 +0100

    Added Agit back into mix

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -2,7 +2,7 @@ M = {
   -- Fugitive
   { "tpope/vim-fugitive" }, -- Fugitive ( Git)
   { "tpope/vim-rhubarb" }, -- Git Support
-
+  { "cohama/agit.vim", }, -- Git history Tree
   -- Git Diffview
   { "sindrets/diffview.nvim" }, -- gitdiff view
   { "nvim-lua/plenary.nvim" },
