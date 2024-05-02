24202d7d87680930d5a85dd458eafae8963383ae
cajone <peat@blueyonder.co.uk>
2024-05-03 00:01:13 +0100

    Configuration update

--- lua/plugins/lsp/debug.lua
+++ lua/plugins/lsp/debug.lua
@@ -58,7 +58,7 @@ M = {
       -- online, please don't ask me how to install them :)
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
-        "delve",
+        "luals",
       },
     })
 
