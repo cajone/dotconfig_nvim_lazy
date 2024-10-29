a6e801153f84774da9720ac1912cb313369041e2
cajone <peat@blueyonder.co.uk>
2024-10-29 08:18:55 +0000

    Reconfigured Mason tools

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -27,9 +27,9 @@ M = {
       ensure_installed = {
         "prettier", -- prettier formatter
         "stylua", -- lua formatter
-        "isort", -- python formatter
-        "black", -- python formatter
-        "pylint", -- python linter
+        --        "isort", -- python formatter
+        --        "black", -- python formatter
+        --        "pylint", -- python linter
         "eslint_d", -- js linter
       },
     })
