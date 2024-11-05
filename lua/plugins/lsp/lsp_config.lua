e32c971c4e16548d3cb92e6d8e5c65c2cce0d223
cajone <peat@blueyonder.co.uk>
2024-11-05 02:19:07 +0000

    Tidied up descriptions on key mappings (telescope)

--- lua/plugins/lsp/lsp_config.lua
+++ lua/plugins/lsp/lsp_config.lua
@@ -21,14 +21,14 @@ M = {
           end
 
           -- Got the definition of the function
-          map("gd", builtin.lsp_definitions, "[G]oto [D]efinition")
+          map("gd", builtin.lsp_definitions, "[g]oto [d]efinition")
 
           -- Find references for the word under your cursor.
-          map("gr", builtin.lsp_references, "[G]oto [R]eferences")
+          map("gr", builtin.lsp_references, "[g]oto [r]eferences")
 
           -- Jump to the implementation of the word under your cursor.
           --  Useful when your language has ways of declaring types without an actual implementation.
-          map("gI", builtin.lsp_implementations, "[G]oto [I]mplementation")
+          map("gI", builtin.lsp_implementations, "[g]oto [I]mplementation")
 
           -- Jump to the type of the word under your cursor.
           --  Useful when you're not sure what type a variable is and you want to see
@@ -37,19 +37,19 @@ M = {
 
           -- Fuzzy find all the symbols in your current document.
           --  Symbols are things like variables, functions, types, etc.
-          map("<leader>ds", builtin.lsp_document_symbols, "[D]ocument [S]ymbols")
+          map("<leader>ds", builtin.lsp_document_symbols, "[d]ocument [s]ymbols")
 
           -- Fuzzy find all the symbols in your current workspace.
           --  Similar to document symbols, except searches over your entire project.
-          map("<leader>ws", builtin.lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+          map("<leader>ws", builtin.lsp_dynamic_workspace_symbols, "[w]orkspace [s]ymbols")
 
           -- Rename the variable under your cursor.
           --  Most Language Servers support renaming across files, etc.
-          map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+          map("<leader>rn", vim.lsp.buf.rename, "[r]e[n]ame")
 
           -- Execute a code action, usually your cursor needs to be on top of an error
           -- or a suggestion from your LSP for this to activate.
-          map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+          map("<leader>ca", vim.lsp.buf.code_action, "[c]ode [a]ction")
 
           -- Opens a popup that displays documentation about the word under your cursor
           --  See `:help K` for why this keymap.
@@ -57,7 +57,7 @@ M = {
 
           -- WARN: This is not Goto Definition, this is Goto Declaration.
           --  For example, in C this would take you to the header.
-          map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+          map("gD", vim.lsp.buf.declaration, "[g]oto [D]eclaration")
 
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
