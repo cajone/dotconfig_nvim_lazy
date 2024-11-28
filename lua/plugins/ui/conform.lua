c6d3ecd1d8ea09e9cdb517a2f2a38e5ecc14876d
cajone <peat@blueyonder.co.uk>
2024-11-28 00:01:11 +0000

    Configuration update

--- lua/plugins/ui/conform.lua
+++ lua/plugins/ui/conform.lua
@@ -6,7 +6,7 @@ M = {
 
     conform.setup({
       formatters_by_ft = {
-        ansible = { "ansible" },
+        ansible = { "ansible_lint" },
         bash = { "beautysh" },
         css = { "prettier" },
         graphql = { "prettier" },
