599e18ac42ce39ad7c0cbc8cf3721d46846ced06
cajone <peat@blueyonder.co.uk>
2024-12-04 19:24:52 +0000

    Refactored Mason plugin config

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -7,9 +7,7 @@ M = {
   config = function()
     -- import mason
     local mason = require("mason") -- Important this must be the first LSP module you load !!
-    -- import mason-lspconfig
     local mason_lspconfig = require("mason-lspconfig")
-    -- import mason-tools
     local mason_tool_installer = require("mason-tool-installer")
 
     mason.setup({
@@ -23,34 +21,22 @@ M = {
     })
 
     mason_lspconfig.setup({
-      -- list of servers for mason to install
       ensure_installed = {
         "ansible-lint",
         "ansiblels",
         "bashls",
-        "beautysh", -- shell lsp
         "groovyls",
         "lua_ls",
-        --        "bashls",
-        --        "pylsp",
       },
-      -- auto-install configured servers ( with lspconfig )
       automatic_installation = true, -- not the same as ensure_installed
     })
 
     mason_tool_installer.setup({
       ensure_installed = {
-        "ansible-lint", -- ansible linter
-        "ansiblels",
-        "bashls",
-        "beautysh", -- shell lsp
-        "eslint_d", -- js linter
-        "groovyls", -- lsp
-        "prettier", -- prettier formatter
-        "stylua", -- lua formatter
-        --        "black", -- python formatter
-        --        "isort", -- python formatter
-        --        "pylint", -- python linter
+        "beautysh",   -- shell lsp
+        "prettier",   -- prettier formatter
+        "shellcheck", -- bash formatter
+        "stylua",     -- lua formatter
       },
     })
   end,
