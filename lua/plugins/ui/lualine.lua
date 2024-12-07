efbf95a7cb20ad7d421fe0e888f237200f7c2dfc
cajone <peat@blueyonder.co.uk>
2024-12-07 14:05:58 +0000

    Refactored

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -2,56 +2,30 @@
 M = {
   "nvim-lualine/lualine.nvim",
   dependencies = {
-    "nvim-tree/nvim-web-devicons",
+    { "nvim-tree/nvim-web-devicons", lazy = true }
   },
-  config = function()
-    local lualine = require("lualine")
-    local lazy_status = require("lazy.status")
-    lualine.setup({
-      options = {
-        icon_enabled = true,
-        component_separators = { left = "|", right = "|" },
-        section_separators = { left = "|", right = "|" },
+  lazy_status = require("lazy.status"),
+  opts = {
+    options = {
+      icon_enabled = true,
+      component_separators = { left = "|", right = "|" },
+      section_separators = { left = "|", right = "|" },
+    },
+    sections = {
+      lualine_a = { "filename", path = 1 },
+      lualine_b = { "branch" },
+      lualine_c = { "diagnostics", sources = { "nvim_lsp" }, symbols = { error = "E", warn = "W", info = "I", hint = "H" }},
+      lualine_x = { },
+      lualine_y = { "progress", "|", "filetype" },
+      lualine_z = {
+        function()
+          return vim.fn.getcwd()
+        end,
+        function()
+          return os.date("%H:%M")
+        end,
       },
-      sections = {
-        lualine_a = {
-          {
-            "filename",
-            path = 1,
-          },
-        },
-        lualine_b = {
-          "branch",
-        },
-        lualine_c = {
-          {
-            "diagnostics",
-            sources = { "nvim_lsp" },
-            symbols = { error = "E", warn = "W", info = "I", hint = "H" },
-          },
-        },
-        lualine_x = {
-          {
-            lazy_status.updates,
-            cond = lazy_status.has_updates,
-            color = { fg = "#ff9e64" },
-          },
-        },
-        lualine_y = {
-          "progress",
-          "|",
-          "filetype",
-        },
-        lualine_z = {
-          function()
-            return vim.fn.getcwd()
-          end,
-          function()
-            return os.date("%H:%M")
-          end,
-        },
-      },
-    })
-  end,
+    },
+  }
 }
 return M
