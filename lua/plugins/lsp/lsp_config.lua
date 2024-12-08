f8ff8547b808af522004c6ab651344d354d26659
cajone <peat@blueyonder.co.uk>
2024-12-08 12:11:13 +0000

    Fix empty lsp_config block

--- lua/plugins/lsp/lsp_config.lua
+++ lua/plugins/lsp/lsp_config.lua
@@ -16,7 +16,9 @@ M = {
 			},
 		},
 		config = function()
-			require("lspconfig").lua_ls.setup()
+			require("lspconfig").lua_ls.setup({
+        diagnostics = { disable = { "missing-fields", "undefined-global" } },
+      })
 		end
 	}
 }
