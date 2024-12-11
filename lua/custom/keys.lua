6015bd73af4fe2f366853cc555a3c29a7d0b3aca
cajone <peat@blueyonder.co.uk>
2024-12-11 00:01:10 +0000

    Configuration update

--- lua/custom/keys.lua
+++ lua/custom/keys.lua
@@ -1,21 +1,23 @@
 -- keys.lua: Person keymaps, leader key is '\' by default
 
+local keymap = vim.api.nvim_set_keymap
+local set = vim.keymap.set
+local builtin = require("telescope.builtin")
+
+
 -- Jumping between windows replacement for Ctrl-w[hjkl]
-vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", { noremap = true })
-vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", { noremap = true })
-vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", { noremap = true })
-vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", { noremap = true })
+keymap("n", "<C-h>", "<C-w>h", { noremap = true })
+keymap("n", "<C-j>", "<C-w>j", { noremap = true })
+keymap("n", "<C-k>", "<C-w>k", { noremap = true })
+keymap("n", "<C-l>", "<C-w>l", { noremap = true })
 
 -- Cut & Paste to system buffer if clipboard is set
-vim.api.nvim_set_keymap("n", "<leader>p", '"+p', { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<leader>p", '"+p', { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>y", '"+yy', { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<leader>y", '"+yy', { noremap = true, silent = true })
+keymap("n", "<leader>p", '"+p', { noremap = true, silent = true })
+keymap("v", "<leader>p", '"+p', { noremap = true, silent = true })
+keymap("n", "<leader>y", '"+yy', { noremap = true, silent = true })
+keymap("v", "<leader>y", '"+yy', { noremap = true, silent = true })
 
 -- Telescope Keys (requires "ripgrep" installed via your system package manager)
-local builtin = require("telescope.builtin")
-local set = vim.keymap.set
-
 set("n", "<leader>fb", builtin.buffers, { desc = "Show currently opened buffers" })
 set("n", "<leader>fc", builtin.grep_string, { desc = "Find string under cursor" })
 set("n", "<leader>ff", builtin.find_files, { desc = "Fuzzy find files in cwd" })
@@ -25,113 +27,113 @@ set("n", "<leader>fr", builtin.oldfiles, { desc = "List recently opened files" }
 set("n", "<leader>km", builtin.keymaps, { desc = "Open keymaps" })
 
 -- GP AI
-vim.api.nvim_set_keymap("n", "<Leader>jaq", ":GpAgent qwen32<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>jaw", ":GpAgent qwq:latest<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>jal", ":GpAgent Llama3.2<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>ja", ":GpAgent", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>jr", ":GpRewrite<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>jd", ":GpChatDelete<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>jf", ":GpChatFinder<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>jA", ":GpAppend<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>jn", ":GpTabnew<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>jp", ":GpPrepend<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>jt", ":GpChatToggle<CR>", { noremap = true })
--- vim.api.nvim_set_keymap("n", "<Leader>jz", ":GpPopup<CR>", { noremap = true })
+keymap("n", "<Leader>jaq", ":GpAgent qwen32<CR>", { noremap = true })
+keymap("n", "<Leader>jaw", ":GpAgent qwq:latest<CR>", { noremap = true })
+keymap("n", "<Leader>jal", ":GpAgent Llama3.2<CR>", { noremap = true })
+keymap("n", "<Leader>ja", ":GpAgent", { noremap = true })
+keymap("n", "<Leader>jr", ":GpRewrite<CR>", { noremap = true })
+keymap("n", "<Leader>jd", ":GpChatDelete<CR>", { noremap = true })
+keymap("n", "<Leader>jf", ":GpChatFinder<CR>", { noremap = true })
+keymap("n", "<Leader>jA", ":GpAppend<CR>", { noremap = true })
+keymap("n", "<Leader>jn", ":GpTabnew<CR>", { noremap = true })
+keymap("n", "<Leader>jp", ":GpPrepend<CR>", { noremap = true })
+keymap("n", "<Leader>jt", ":GpChatToggle<CR>", { noremap = true })
+-- keym("n", "<Leader>jz", ":GpPopup<CR>", { noremap = true })
 
 -- Obsidian
-vim.api.nvim_set_keymap("n", "<Leader>oS", ":ObsidianQuickSwitch<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>of", ":ObsidianFollowLink vsplit<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>ol", ":ObsidianLinks<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>on", ":ObsidianNew", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>op", ":ObsidianOpen<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>os", ":ObsidianSearch<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>ot", ":ObsidianTags", { noremap = true })
+keymap("n", "<Leader>oS", ":ObsidianQuickSwitch<CR>", { noremap = true })
+keymap("n", "<Leader>of", ":ObsidianFollowLink vsplit<CR>", { noremap = true })
+keymap("n", "<Leader>ol", ":ObsidianLinks<CR>", { noremap = true })
+keymap("n", "<Leader>on", ":ObsidianNew", { noremap = true })
+keymap("n", "<Leader>op", ":ObsidianOpen<CR>", { noremap = true })
+keymap("n", "<Leader>os", ":ObsidianSearch<CR>", { noremap = true })
+keymap("n", "<Leader>ot", ":ObsidianTags", { noremap = true })
 
 -- Remote SSH connections
-vim.api.nvim_set_keymap("n", "<Leader>rs", ":RemoteStart<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>rS", ":RemoteStop<CR>", { noremap = true })
+keymap("n", "<Leader>rs", ":RemoteStart<CR>", { noremap = true })
+keymap("n", "<Leader>rS", ":RemoteStop<CR>", { noremap = true })
 
 -- :only
-vim.api.nvim_set_keymap("n", "<Leader>0", ":only<CR>",
+keymap("n", "<Leader>0", ":only<CR>",
   { desc = "Close all buffers except current focus", noremap = true, silent = true })
 
 -- Git
-vim.api.nvim_set_keymap('n', '<leader>Gp', ':lua push_current_branch()<cr>', { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>gf", ":FugitiveInside<CR>", { noremap = true })
-vim.api.nvim_set_keymap(
+keymap('n', '<leader>Gp', ':lua push_current_branch()<cr>', { noremap = true })
+keymap("n", "<Leader>gf", ":FugitiveInside<CR>", { noremap = true })
+keymap(
   "n",
   "<Leader>gls",
   ':G log --pretty=format:"%h - %an, %ar : %s"<CR> ', { desc = "Short formatted git log", noremap = true })
 
 -- Clean up code
-vim.api.nvim_set_keymap("n", "<Leader>ec", "Extract_selected_code_blocks()<CR>", { noremap = true })
+keymap("n", "<Leader>ec", "Extract_selected_code_blocks()<CR>", { noremap = true })
 
 -- Toggle Line Numbers
-vim.api.nvim_set_keymap("n", "<Leader>nn", ":lua ToggleLineNumbers()<CR>", { noremap = true })
+keymap("n", "<Leader>nn", ":lua ToggleLineNumbers()<CR>", { noremap = true })
 
 -- Dismiss notices
-vim.api.nvim_set_keymap("n", "<leader>Nn", "<cmd>NoiceDismiss<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>Ne", "<cmd>NoiceEnable<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>Nd", "<cmd>NoiceDisable<CR>", { noremap = true, silent = true })
+keymap("n", "<leader>Nn", "<cmd>NoiceDismiss<CR>", { noremap = true, silent = true })
+keymap("n", "<leader>Ne", "<cmd>NoiceEnable<CR>", { noremap = true, silent = true })
+keymap("n", "<leader>Nd", "<cmd>NoiceDisable<CR>", { noremap = true, silent = true })
 
 --- FzfLua
-vim.api.nvim_set_keymap("n", "<leader>gb", "<cmd>FzfLua git_branches<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>gh", "<cmd>FzfLua helptags<CR>", { noremap = true, silent = true })
+keymap("n", "<leader>gb", "<cmd>FzfLua git_branches<CR>", { noremap = true, silent = true })
+keymap("n", "<leader>gh", "<cmd>FzfLua helptags<CR>", { noremap = true, silent = true })
 
 -- Clear current search
-vim.api.nvim_set_keymap("n", "<Leader>z", ":nohlsearch<CR>", { desc = "Clear current search", noremap = true })
+keymap("n", "<Leader>z", ":nohlsearch<CR>", { desc = "Clear current search", noremap = true })
 
 -- Refresh the nvim configuration after modification
-vim.api.nvim_set_keymap("n", "<Leader>sf", "<cmd>source %<CR>",
+keymap("n", "<Leader>sf", "<cmd>source %<CR>",
   { desc = "Reloads the configuration after a change", noremap = true }
 )
 
 -- Toggle nvim-tree, load the DirectoryFiles
-vim.api.nvim_set_keymap("n", "<Leader>df", ":NvimTreeToggle()<CR>", { desc = "Toggle Nvimtree", noremap = true })
+keymap("n", "<Leader>df", ":NvimTreeToggle()<CR>", { desc = "Toggle Nvimtree", noremap = true })
 
 -- Format buffer
-vim.api.nvim_set_keymap("n", "<Leader>NF", ":lua vim.lsp.buf.format()<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>NL", ":lua vim.lsp.buf.linting()<CR>", { noremap = true })
+keymap("n", "<Leader>NF", ":lua vim.lsp.buf.format()<CR>", { noremap = true })
+keymap("n", "<Leader>NL", ":lua vim.lsp.buf.linting()<CR>", { noremap = true })
 
 -- <F> Keys
 -- resize current focused vertical panel by 5 chars
-vim.api.nvim_set_keymap("n", "<F3>", ":vertical resize -5<CR>",
+keymap("n", "<F3>", ":vertical resize -5<CR>",
   { desc = "Descrease current window size by 5 chars", noremap = true }
 )
-vim.api.nvim_set_keymap("n", "<F4>", ":vertical resize +5<CR>",
+keymap("n", "<F4>", ":vertical resize +5<CR>",
   { desc = "Increase current window size by 5 chars", noremap = true }
 )
 
 -- Spelling
 -- F5 will find next word in doc,
-vim.api.nvim_set_keymap("n", "<F5>", "]s", { desc = "Spell check txt/md files", noremap = true })
+keymap("n", "<F5>", "]s", { desc = "Spell check txt/md files", noremap = true })
 
 -- F6 will prompt to select from a dictionary
-vim.api.nvim_set_keymap("n", "<F6>", "z=", { desc = "Open Dictionary options for word under cursor", noremap = true })
+keymap("n", "<F6>", "z=", { desc = "Open Dictionary options for word under cursor", noremap = true })
 
 -- F7 will add word to local dictionary
-vim.api.nvim_set_keymap("n", "<F7>", "zg", { desc = "Save a word to a local dictionary", noremap = true })
+keymap("n", "<F7>", "zg", { desc = "Save a word to a local dictionary", noremap = true })
 
 -- Toggle Vimwiki URL's
-vim.api.nvim_set_keymap("n", "<F8>", ":lua ToggleVimwikiConceallevel()<CR>",
+keymap("n", "<F8>", ":lua ToggleVimwikiConceallevel()<CR>",
   { desc = "Show url paths in markdown files", noremap = true }
 )
 
 -- Mapping for windo diffs
-vim.api.nvim_set_keymap("n", "<leader>wd", ":windo diffthis<CR>",
+keymap("n", "<leader>wd", ":windo diffthis<CR>",
   { desc = "Diff compare open split buffers", noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>wD", ":windo diffoff<CR>", { desc = "Diff compare off", noremap = true })
+keymap("n", "<leader>wD", ":windo diffoff<CR>", { desc = "Diff compare off", noremap = true })
 
 -- Mapping for cookstyle
-vim.api.nvim_set_keymap("n", "<leader>cs", ":!/opt/chefkdk/embedded/bin/cookstyle -a %<CR>", { noremap = true })
+keymap("n", "<leader>cs", ":!/opt/chefkdk/embedded/bin/cookstyle -a %<CR>", { noremap = true })
 
 -- Map a key combination to open Vimwiki and set LCD
-vim.api.nvim_set_keymap("n", "<Leader>ww", ":lua OpenVimwiki()<CR>",
+keymap("n", "<Leader>ww", ":lua OpenVimwiki()<CR>",
   { desc = "Open Wiki", noremap = true, silent = true }
 )
 
 -- Set LCD
-vim.api.nvim_set_keymap(
+keymap(
   "n",
   "<Leader>lcd",
   [[:lua ChangeWorkingDirectoryToGitRoot()<CR>]],
@@ -139,11 +141,11 @@ vim.api.nvim_set_keymap(
 )
 
 -- Add a mapping to open plugin help
-vim.api.nvim_set_keymap("n", "<Leader>ph", [[:lua OpenPluginHelp()<CR>]], { silent = true })
+keymap("n", "<Leader>ph", [[:lua OpenPluginHelp()<CR>]], { silent = true })
 
 -- Tabs
-vim.api.nvim_set_keymap("n", "<leader>tp", ":tabprevious<CR>",
+keymap("n", "<leader>tp", ":tabprevious<CR>",
   { desc = "Select Previous Tab", noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>tn", ":tabNext<CR>", { desc = "Select Next Tab", noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>tN", ":tabnew<CR>", { desc = "Open New Tab", noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>tc", ":tabclose<CR>", { desc = "Close Current Tab", noremap = true, silent = true })
+keymap("n", "<leader>tn", ":tabNext<CR>", { desc = "Select Next Tab", noremap = true, silent = true })
+keymap("n", "<leader>tN", ":tabnew<CR>", { desc = "Open New Tab", noremap = true, silent = true })
+keymap("n", "<leader>tc", ":tabclose<CR>", { desc = "Close Current Tab", noremap = true, silent = true })
--- lua/custom/opts.lua
+++ lua/custom/opts.lua
@@ -4,15 +4,17 @@
 vim.api.nvim_create_autocmd('FileType', {
   pattern = 'lua',
   callback = function()
-    vim.bo.tabstop = 2        -- Set tabstop to 2 for Lua files
-    vim.bo.shiftwidth = 2     -- Set shiftwidth to 2 for Lua files
-    vim.bo.softtabstop = 2    -- Set softtabstop to 2 for Lua files, needed for consistent behavior
-    vim.bo.expandtab = true   -- Ensure that spaces are used instead of tabs for indentation in Lua files
+    vim.bo.tabstop = 2      -- Set tabstop to 2 for Lua files
+    vim.bo.shiftwidth = 2   -- Set shiftwidth to 2 for Lua files
+    vim.bo.softtabstop = 2  -- Set softtabstop to 2 for Lua files, needed for consistent behavior
+    vim.bo.expandtab = true -- Ensure that spaces are used instead of tabs for indentation in Lua files
   end,
 })
 
 local opt = vim.opt
 local g = vim.g
+
+-- g.makeprg = 'ansible- lint'
 
 -- Sync clipboard between OS and Neovim.
 -- Remove this option if you want your OS clipboard to remain independent.
@@ -32,46 +34,46 @@ g.loaded_netrwPlugin = 1
 
 -- [[ Context ]]
 -- opt.colorcolumn = "120" -- str: Show column for max line length
-opt.number = true -- bool: Show line numbers
+opt.number = true         -- bool: Show line numbers
 opt.relativenumber = true -- bool: Show relative line numbers
-opt.scrolloff = 4 -- int: Min num lines of context
-opt.signcolumn = "yes" -- str: Show the sign column
-opt.scrolljump = 5 -- Lines to scroll when cursor leaves screen
+opt.scrolloff = 4         -- int: Min num lines of context
+opt.signcolumn = "yes"    -- str: Show the sign column
+opt.scrolljump = 5        -- Lines to scroll when cursor leaves screen
 
 -- [[ Filetypes ]]
 opt.encoding = "utf8" -- str: String encoding to use
 
 -- [[ Theme ]]
-opt.syntax = "ON" -- str: Allow syntax highlighting
+opt.syntax = "ON"        -- str: Allow syntax highlighting
 opt.termguicolors = true -- bool: If term supports UI color then enable
 
 -- [[ Search ]]
 opt.ignorecase = true -- bool: Ignore case in search patterns
-opt.smartcase = true -- bool: Override ignorecase if search contains capitals
-opt.incsearch = true -- bool: Use incremental search
-opt.hlsearch = true -- bool: Highlight search matches
-opt.showmatch = true -- bool: Show matching brackets/parenthesis
+opt.smartcase = true  -- bool: Override ignorecase if search contains capitals
+opt.incsearch = true  -- bool: Use incremental search
+opt.hlsearch = true   -- bool: Highlight search matches
+opt.showmatch = true  -- bool: Show matching brackets/parenthesis
 
 -- [[ Whitespace ]]
 opt.expandtab = true -- bool: Use spaces instead of tabs
-opt.shiftwidth = 2 -- num: Size of an indent
-opt.softtabstop = 2 -- num: Number of spaces tabs count for in insert mode
-opt.tabstop = 2 -- num: Number of spaces tabs count for
+opt.shiftwidth = 2   -- num: Size of an indent
+opt.softtabstop = 2  -- num: Number of spaces tabs count for in insert mode
+opt.tabstop = 2      -- num: Number of spaces tabs count for
 
 -- [[ Splits ]]
 opt.splitright = true -- bool: Place new window to the right of current one
 opt.splitbelow = true -- bool: Place new window below the current one
 
 -- [[ Maintain undo history between sessions :echo g:undodir ]]
-opt.undofile = true -- Allow undo history to be kept
+opt.undofile = true                -- Allow undo history to be kept
 g.undodir = "~/.config/nvim/undo/" -- Where to keep the undo history
-opt.viminfo:append({ "n" }) -- Remember cursor position between sessions
-opt.swapfile = false -- Don't generate a swap file
+opt.viminfo:append({ "n" })        -- Remember cursor position between sessions
+opt.swapfile = false               -- Don't generate a swap file
 
 -- [[ Ctrl-n menu options ]]
-opt.wildmenu = true -- Show list instead of just completing
+opt.wildmenu = true                -- Show list instead of just completing
 opt.wildmode = "list:longest,full" -- Command <Tab> completion, list matches, then longest common part, then all.
-opt.whichwrap = "b,s,h,l,<,>,[,]" -- Backspace and cursor keys wrap too
+opt.whichwrap = "b,s,h,l,<,>,[,]"  -- Backspace and cursor keys wrap too
 
 -- [[ Change the default shell out to /usr/bin/bash ]]
 opt.shell = "/usr/bin/bash" -- Change the :! shell to use bash
