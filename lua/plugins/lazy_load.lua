2b2304b89ee599966ac7b97a3ba8e2e9916599cc
cajone <peat@blueyonder.co.uk>
2024-12-11 20:15:33 +0000

    Removed excessive plugins for a minimal requirement

--- lua/plugins/lazy_load.lua
+++ lua/plugins/lazy_load.lua
@@ -29,47 +29,19 @@ require("lazy").setup({
   { "instant-markdown/vim-instant-markdown" }, -- Instant Markdown
   { "vimwiki/vimwiki" },                      -- Vimwiki
 
-  --  { import = "plugins.lazydev" },
-  --  { import = "plugins" },
-  --  { import = "plugins.ui" },
-  --  { import = "plugins.lsp" },
-  --  { import = "plugins.ai" },
-
   { import = "plugins.obsidian" }, -- obsidian note taker
   { import = "plugins.treesitter" },
   -- 	{ import = "plugins.lint" },      -- Linter(s)
 
   { import = "plugins.git" },     -- git related plugins
-  --	{ import = "plugins.render-markdown" }, -- Better Terminal display of markdown
   { import = "plugins.terminal" }, -- Toggle Terminal window
-  -- 	{ import = "plugins.markdown-preview" }, -- Instant Markdown for neovim
 
   -- UI based plugins
   { import = "plugins.ui.colorscheme" }, -- ColorScheme
   { import = "plugins.ui.dressing" }, -- Allows prompts and selections
   { import = "plugins.ui.lualine" },    -- Status Line
-  -- { import = "plugins.ui.mini" }, -- Collections on notes, todo's
   { import = "plugins.ui.nvimtree" },   -- NvimTree
   { import = "plugins.ui.telescope" },  -- Fuzzy file finder and many other things
-  --	{ import = "plugins.ui.todo" }, -- Todo notes etc
-  -- 	-- { import = "plugins.ui.noice" }, --  System messages popup window
-  --	{ import = "plugins.ui.conform" }, -- Formatting, linting
   { import = "plugins.ui.fzf-lua" }, -- Fuzzy Finding
-
-  -- AI based plugins
-  --	{ import = "plugins.ai.gp" }, -- Configure AI prompt
-  -- 	{ import = "plugins.ai.ai" }, -- Configure AI prompt
-  -- 	{ import = "plugins.ai.copilot" }, -- Configure AI prompt
-
-  -- LSP / Autocompletion language Plugins
-  -- 	{ import = "plugins.lsp.mason" }, -- LSP installer : NOTE THIS HAS TO BE THE FIRST LSP FILE TO LOAD!!!
-  --	{ import = "plugins.lsp.none-ls" }, -- null-ls replacement
-  --	{ import = "plugins.lsp.nvim-cmp" }, -- Auto Completion
-  --	{ import = "plugins.lsp.debug" }, -- LSP Debug
-  --	{ import = "plugins.lsp.lsp_config" }, -- LSP configuration
-
-  -- DAP Debugging code base
-  -- { "mfussenegger/nvim-dap" },
-  -- { "jbyuki/one-small-step-for-vimkind" },
 
 })
