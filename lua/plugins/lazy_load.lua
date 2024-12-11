1877204c0cc3939a1872940d042eaf25194604e2
cajone <peat@blueyonder.co.uk>
2024-12-11 21:23:37 +0000

    Tried to configure lsp to remove undefined global vim lsp warning

--- lua/plugins/lazy_load.lua
+++ lua/plugins/lazy_load.lua
@@ -1,75 +1,75 @@
 -- OK first setup the plugin manager "Lazy"
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	checker = { -- turns off notifications
-		enable = true,
-		notify = false,
-	},
-	defaults = {
-		lazy = false,
-		version = false,
-	},
+  checker = { -- turns off notifications
+    enable = true,
+    notify = false,
+  },
+  defaults = {
+    lazy = false,
+    version = false,
+  },
 
-	change_detection = { notify = false }, -- Stop reporting auto changes
+  change_detection = { notify = false }, -- Stop reporting auto changes
 
-	-- Plugins used straight out the box
-	{ "instant-markdown/vim-instant-markdown" }, -- Instant Markdown
-	{ "vimwiki/vimwiki" },                -- Vimwiki
+  -- Plugins used straight out the box
+  { "instant-markdown/vim-instant-markdown" }, -- Instant Markdown
+  { "vimwiki/vimwiki" },                       -- Vimwiki
 
-	--  { import = "plugins.lazydev" },
-	--  { import = "plugins" },
-	--  { import = "plugins.ui" },
-	--  { import = "plugins.lsp" },
-	--  { import = "plugins.ai" },
+  --  { import = "plugins.lazydev" },
+  --  { import = "plugins" },
+  --  { import = "plugins.ui" },
+  --  { import = "plugins.lsp" },
+  --  { import = "plugins.ai" },
 
-	{ import = "plugins.obsidian" },  -- obsidian note taker
-	{ import = "plugins.treesitter" },
--- 	{ import = "plugins.lint" },      -- Linter(s)
- 
-	{ import = "plugins.git" },       -- git related plugins
-	{ import = "plugins.render-markdown" }, -- Better Terminal display of markdown
-	{ import = "plugins.terminal" },  -- Toggle Terminal window
--- 	{ import = "plugins.markdown-preview" }, -- Instant Markdown for neovim
- 
- 	-- UI based plugins
-	{ import = "plugins.ui.colorscheme" }, -- ColorScheme
-	{ import = "plugins.ui.dressing" }, -- Allows prompts and selections
-	{ import = "plugins.ui.lualine" }, -- Status Line
- 	-- { import = "plugins.ui.mini" }, -- Collections on notes, todo's
-	{ import = "plugins.ui.nvimtree" }, -- NvimTree
-	{ import = "plugins.ui.telescope" }, -- Fuzzy file finder and many other things
-	{ import = "plugins.ui.todo" }, -- Todo notes etc
--- 	-- { import = "plugins.ui.noice" }, --  System messages popup window
-	{ import = "plugins.ui.conform" }, -- Formatting, linting
-	{ import = "plugins.ui.fzf-lua" }, -- Formatting, linting
- 
- 	-- AI based plugins
-	{ import = "plugins.ai.gp" }, -- Configure AI prompt
--- 	{ import = "plugins.ai.ai" }, -- Configure AI prompt
--- 	{ import = "plugins.ai.copilot" }, -- Configure AI prompt
- 
- 	-- LSP / Autocompletion language Plugins
- 	{ import = "plugins.lsp.mason" }, -- LSP installer : NOTE THIS HAS TO BE THE FIRST LSP FILE TO LOAD!!!
-	{ import = "plugins.lsp.none-ls" }, -- null-ls replacement
-	{ import = "plugins.lsp.nvim-cmp" }, -- Auto Completion
-	{ import = "plugins.lsp.debug" }, -- LSP Debug
-	{ import = "plugins.lsp.lsp_config" }, -- LSP configuration
+  { import = "plugins.obsidian" }, -- obsidian note taker
+  { import = "plugins.treesitter" },
+  -- 	{ import = "plugins.lint" },      -- Linter(s)
 
-	-- DAP Debugging code base
-	-- { "mfussenegger/nvim-dap" },
-	-- { "jbyuki/one-small-step-for-vimkind" },
+  { import = "plugins.git" },             -- git related plugins
+  { import = "plugins.render-markdown" }, -- Better Terminal display of markdown
+  { import = "plugins.terminal" },        -- Toggle Terminal window
+  -- 	{ import = "plugins.markdown-preview" }, -- Instant Markdown for neovim
+
+  -- UI based plugins
+  { import = "plugins.ui.colorscheme" }, -- ColorScheme
+  { import = "plugins.ui.dressing" },    -- Allows prompts and selections
+  { import = "plugins.ui.lualine" },     -- Status Line
+  { import = "plugins.ui.mini" },        -- Collections on notes, todo's
+  { import = "plugins.ui.nvimtree" },    -- NvimTree
+  { import = "plugins.ui.telescope" },   -- Fuzzy file finder and many other things
+  { import = "plugins.ui.todo" },        -- Todo notes etc
+  { import = "plugins.ui.noice" },       --  System messages popup window
+  { import = "plugins.ui.conform" },     -- Formatting, linting
+  --	{ import = "plugins.ui.fzf-lua" }, -- Formatting, linting
+
+  -- AI based plugins
+  { import = "plugins.ai.gp" }, -- Configure AI prompt
+  -- 	{ import = "plugins.ai.ai" }, -- Configure AI prompt
+  -- 	{ import = "plugins.ai.copilot" }, -- Configure AI prompt
+
+  -- LSP / Autocompletion language Plugins
+  { import = "plugins.lsp.mason" },      -- LSP installer : NOTE THIS HAS TO BE THE FIRST LSP FILE TO LOAD!!!
+  { import = "plugins.lsp.none-ls" },    -- null-ls replacement
+  { import = "plugins.lsp.nvim-cmp" },   -- Auto Completion
+  { import = "plugins.lsp.debug" },      -- LSP Debug
+  { import = "plugins.lsp.lsp_config" }, -- LSP configuration
+
+  -- DAP Debugging code base
+  -- { "mfussenegger/nvim-dap" },
+  -- { "jbyuki/one-small-step-for-vimkind" },
 
 })
--- lua/plugins/ui/noice.lua
+++ lua/plugins/ui/noice.lua
@@ -8,50 +8,50 @@ M = {
   },
   opts = {
     presets = {
-      bottom_search = false, -- Use classic bottom cmdline for search/ibhagwan/fzf-lua
-      command_palette = false, -- Position cmdline and popupmenu together (optional)
+      bottom_search = false,        -- Use classic bottom cmdline for search/ibhagwan/fzf-lua
+      command_palette = false,      -- Position cmdline and popupmenu together (optional)
       long_message_to_split = true, -- Send long messages to a split
-      inc_rename = true, -- Enable input dialog for inc-rename.nvim
-      lsp_doc_border = true, -- Add border to hover docs and signature help
+      inc_rename = true,            -- Enable input dialog for inc-rename.nvim
+      lsp_doc_border = true,        -- Add border to hover docs and signature help
     },
     lsp = {
       override = {
         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
         ["vim.lsp.util.stylize_markdown"] = true,
-        ["cmp.entry.get_documentation"] = true, -- Requires hrsh7th/nvim-cmp
+        ["cmp.entry.get_documentation"] = true,          -- Requires hrsh7th/nvim-cmp
       },
-      hover = { enabled = true, view = nil }, -- Use default view or custom one
+      hover = { enabled = true, view = nil },            -- Use default view or custom one
       signature = { enabled = true, auto_open = false }, -- Auto open on cursor movement (optional)
     },
     views = {
       popupmenu = {
-        border = true, -- Add border around the popup menu
+        border = true,                                                                       -- Add border around the popup menu
         win_options = {
           winhighlight = "Normal:Normal,FloatBorder:NoicePopupmenuBorder,CursorLine:Normal", -- Customize window highlights
         },
       },
     },
     messages = { enabled = true, view_search = false }, -- Use dedicated search view (optional)
-    statusmsg = { enabled = true, view = "mini" }, -- Status bar integration (optional)
+    statusmsg = { enabled = true, view = "mini" },      -- Status bar integration (optional)
     cmd_palette = {
-      enabled = false, -- Enable command palette
+      enabled = false,                                  -- Enable command palette
       position = { row = 10, col = 4 },
-      border = true, -- Add border around the command palette
+      border = true,                                    -- Add border around the command palette
       keymap = {
-        next = "<C-j>", -- Next item in command palette
-        prev = "<C-k>", -- Previous item in command palette
-        confirm = "<CR>", -- Confirm selection in command palette
-        cancel = "<Esc>", -- Cancel selection in command palette
+        next = "<C-j>",                                 -- Next item in command palette
+        prev = "<C-k>",                                 -- Previous item in command palette
+        confirm = "<CR>",                               -- Confirm selection in command palette
+        cancel = "<Esc>",                               -- Cancel selection in command palette
       },
     },
     routes = {
       { filter = { event = "msg_show", find = "written" }, view = "mini" }, -- Route messages to mini view
-      { filter = { event = "lsp_progress" }, view = "mini" }, -- Route LSP progress to mini view
+      { filter = { event = "lsp_progress" },               view = "mini" }, -- Route LSP progress to mini view
     },
     notify = {
       enabled = true, -- Enable notifications
       timeout = 5000, -- Set notification timeout (ms)
-      level = 2, -- Notifications level (1: error, 2: warn, 3: info, 4: debug)
+      level = 1,      -- Notifications level (1: error, 2: warn, 3: info, 4: debug)
     },
     dismiss = {
       enabled = true, -- Enable message dismissal based on events
