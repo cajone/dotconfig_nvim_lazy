765c1f9d6dbc1dd5ac22eb536768a6450bf94ff6
cajone <peat@blueyonder.co.uk>
2024-12-12 22:23:26 +0000

    Refactored layout, set theme to nord

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -1,55 +1,54 @@
 -- Lualine Setup
 M = {
-	"nvim-lualine/lualine.nvim",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
-	config = function()
-		local lualine = require("lualine")
-		local lazy_status = require("lazy.status")
-		lualine.setup({
-			options = {
-				icon_enabled = true,
-				component_separators = { left = "|", right = "|" },
-				section_separators = { left = "|", right = "|" },
-			},
-			sections = {
-				lualine_a = {
-					{
-						"filename",
-						path = 1,
-					},
-				},
-				lualine_b = {
-					"branch",
-				},
-				lualine_c = {
-					{
-						"diagnostics",
-						sources = { "nvim_lsp" },
-						symbols = { error = "E", warn = "W", info = "I", hint = "H" },
-					},
-				},
-				lualine_x = {
-					{
-						lazy_status.updates,
-						cond = lazy_status.has_updates,
-						color = { fg = "#ff9e64" },
-					},
-				},
-				lualine_y = {
-					"filetype",
-				},
-				lualine_z = {
-					function()
-						return vim.fn.getcwd()
-					end,
-					function()
-						return os.date("%H:%M")
-					end,
-				},
-			},
-		})
-	end,
+  "nvim-lualine/lualine.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    local lualine = require("lualine")
+    local lazy_status = require("lazy.status")
+    lualine.setup({
+      options = {
+        theme = 'nord',
+        icon_enabled = true,
+        section_separators = { left = '', right = '' },
+        component_separators = { left = '', right = '' }
+        --  component_separators = { left = "|", right = "|" },
+        --  section_separators = { left = "|", right = "|" },
+      },
+      sections = {
+        lualine_a = {
+          {
+            "filename",
+            path = 1,
+          },
+        },
+        lualine_b = {
+          "branch",
+          "diff",
+          "diagnostics"
+        },
+        lualine_c = {},
+        lualine_x = {
+          {
+            lazy_status.updates,
+            cond = lazy_status.has_updates,
+            color = { fg = "#ff9e64" },
+          },
+        },
+        lualine_y = {
+          "filetype",
+        },
+        lualine_z = {
+          function()
+            return vim.fn.getcwd()
+          end,
+          function()
+            return os.date("%H:%M")
+          end,
+        },
+      },
+    })
+  end,
 }
 return M
