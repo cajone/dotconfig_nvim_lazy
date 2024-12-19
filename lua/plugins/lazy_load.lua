fc75f31216f8d39ed58022bbab75e87061ccf382
cajone <peat@blueyonder.co.uk>
2024-12-19 01:37:39 +0000

    Folding

--- lua/plugins/lazy_load.lua
+++ lua/plugins/lazy_load.lua
@@ -1,4 +1,4 @@
--- OK first setup the plugin manager "Lazy"
+-- Setup the plugin manager "Lazy"
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -10,6 +10,7 @@ if not vim.loop.fs_stat(lazypath) then
     lazypath,
   })
 end
+
 
 vim.opt.rtp:prepend(lazypath)
 
@@ -29,20 +30,16 @@ require("lazy").setup({
   { "instant-markdown/vim-instant-markdown" }, -- Instant Markdown
   { "vimwiki/vimwiki" },                       -- Vimwiki
 
-  --  { import = "plugins.lazydev" },
-  --  { import = "plugins" },
-  --  { import = "plugins.ui" },
-  --  { import = "plugins.lsp" },
-  --  { import = "plugins.ai" },
 
+  -- General plugins
   { import = "plugins.obsidian" }, -- obsidian note taker
   { import = "plugins.treesitter" },
   -- 	{ import = "plugins.lint" },      -- Linter(s)
-
   { import = "plugins.git" },             -- git related plugins
   { import = "plugins.render-markdown" }, -- Better Terminal display of markdown
   { import = "plugins.terminal" },        -- Toggle Terminal window
   -- 	{ import = "plugins.markdown-preview" }, -- Instant Markdown for neovim
+
 
   -- UI based plugins
   { import = "plugins.ui.colorscheme" }, -- ColorScheme
@@ -54,12 +51,15 @@ require("lazy").setup({
   { import = "plugins.ui.todo" },        -- Todo notes etc
   { import = "plugins.ui.noice" },       --  System messages popup window
   { import = "plugins.ui.conform" },     -- Formatting, linting
+  --  { import = "plugins.ui.ufo" },         -- Modern folding
   --	{ import = "plugins.ui.fzf-lua" }, -- Formatting, linting
+
 
   -- AI based plugins
   { import = "plugins.ai.gp" }, -- Configure AI prompt
   -- 	{ import = "plugins.ai.ai" }, -- Configure AI prompt
   -- 	{ import = "plugins.ai.copilot" }, -- Configure AI prompt
+
 
   -- LSP / Autocompletion language Plugins
   { import = "plugins.lsp.mason" },      -- LSP installer : NOTE THIS HAS TO BE THE FIRST LSP FILE TO LOAD!!!
---
+++ lua/plugins/ui/ufo.lua
@@ -0,0 +1,7 @@
+M = {
+  "kevinhwang91/nvim-ufo",
+  dependencies = {
+    'kevinhwang91/promise-async',
+  },
+}
+return M
