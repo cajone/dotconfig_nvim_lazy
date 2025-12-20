0af3a9d7cd1dc0c3a5b36509f0ab41be51412df9
cajone <peat@blueyonder.co.uk>
2025-12-20 00:01:09 +0000

    Configuration update

--- lua/plugins/ai/copilot.lua
+++ lua/plugins/ai/copilot.lua
@@ -1,5 +1,13 @@
 local M = {
   "github/copilot.vim",
   "CopilotC-Nvim/CopilotChat.nvim",
+  dependencies = {
+    { "nvim-lua/plenary.nvim", branch = "master" },
+  },
+  build = "make tiktoken",
+  opts = {
+    -- See Configuration section for options
+  },
 }
+
 return M
