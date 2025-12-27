87210fe72eb61746a73a87d28615fd9e6e1d665f
cajone <peat@blueyonder.co.uk>
2025-12-27 11:13:06 +0000

    Added after load scm file ( not working )

---
+++ after/queries/markdown/highlights.scm
@@ -0,0 +1,2 @@
+;; extends
+(link_destination) @conceal
--- lua/plugins/neowiki.lua
+++ lua/plugins/neowiki.lua
@@ -2,47 +2,49 @@ local M = {
   "echaya/neowiki.nvim",
   dependencies = {
     "MeanderingProgrammer/render-markdown.nvim",
-    "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
   },
   init = function()
-    -- Global settings
-    vim.opt.conceallevel = 2
-    vim.opt.concealcursor = "nc"
+    -- Your Toggle Function (Modernized)
+    _G.ToggleWikiConceal = function()
+      local level = vim.opt_local.conceallevel:get()
+      if level == 0 then
+        vim.opt_local.conceallevel = 2
+        print("Conceal: ON")
+      else
+        vim.opt_local.conceallevel = 0
+        print("Conceal: OFF")
+      end
+    end
 
-    -- Ensure these settings stick specifically for markdown files
+    -- Create an Autocmd to set defaults for wiki/markdown files
     vim.api.nvim_create_autocmd("FileType", {
       pattern = { "markdown", "neowiki" },
       callback = function()
         vim.opt_local.conceallevel = 2
-        vim.opt_local.concealcursor = "nc"
+        -- Keybind to toggle concealment while in a wiki file
+        vim.keymap.set("n", "<leader>tc", _G.ToggleWikiConceal, { buffer = true, desc = "Toggle Wiki Conceal" })
       end,
     })
   end,
-  opts = {
-    wiki_dirs = {
-      { name = "Work", path = "~/vimwiki" },
-    },
-  },
-  -- Configure render-markdown to force concealment
   config = function(_, opts)
     require("neowiki").setup(opts)
     require("render-markdown").setup({
+      -- This tells the renderer to squash the [Label](URL) into just [Label]
       link = {
-        -- This is the key setting to hide the (https://...) part
         enabled = true,
-        render_mode = "compact", -- Hides the URL and brackets
-      },
-      -- Some colorschemes override conceal highlights; force it here if needed
-      conceal = {
-        enabled = true,
+        render_mode = "compact",
       },
     })
   end,
+
   keys = {
     { "<leader>ww", "<cmd>lua require('neowiki').open_wiki()<cr>",          desc = "Open Wiki" },
-    { "<leader>wW", "<cmd>lua require('neowiki').open_wiki_floating()<cr>", desc = "Open Wiki in Floating Window" },
-    { "<leader>wT", "<cmd>lua require('neowiki').open_wiki_new_tab()<cr>",  desc = "Open Wiki in Tab" },
+    { "<leader>wW", "<cmd>lua require('neowiki').open_wiki_floating()<cr>", desc = "Open Wiki (Float)" },
+    { "<leader>wT", "<cmd>lua require('neowiki').open_wiki_new_tab()<cr>",  desc = "Open Wiki (Tab)" },
+  },
+
+  opts = {
+    wiki_dirs = { { name = "Work", path = "~/vimwiki" } },
   },
 }
 
