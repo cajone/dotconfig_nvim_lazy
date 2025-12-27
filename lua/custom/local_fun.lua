1f5c346840299035d4e029157be24d0493357f4c
cajone <peat@blueyonder.co.uk>
2025-12-27 11:00:18 +0000

    Refactored

--- lua/custom/local_fun.lua
+++ lua/custom/local_fun.lua
@@ -15,44 +15,6 @@ function ToggleVimwikiConceallevel()
   end
 end
 
-
-
--- GIt functions
-
--- Define a function to push the current branch
-function _G.push_current_branch()
-    local branch_name = vim.fn.systemlist("git rev-parse --abbrev-ref HEAD")[1]
-    if not branch_name then
-        print("Could not determine the current branch.")
-        return
-    end
-
-    -- Construct and execute the git push command
-    local push_command = string.format('git push -u origin %s', branch_name)
-    local handle = io.popen(push_command)
-    local exit_status = handle:read("*a")
-    handle:close()
-
-    print(exit_status)
-end
-
-
-
--- Change the LCD to the current Git buffer root
-function ChangeWorkingDirectoryToGitRoot()
-  local buffer_directory = vim.fn.expand("%:p:h")
-  local git_root_path = nil
-
-  -- Run git rev-parse to find the root of the repository
-  git_root_path = vim.fn.systemlist("git -C " .. buffer_directory .. " rev-parse --show-toplevel")[1]
-
-  if git_root_path then
-    require("nvim-tree").change_dir(git_root_path)
-  else
-    print("Not in a Git repository")
-  end
-end
-
 -- Open Vimwiki and set LCD
 function OpenVimwiki()
   vim.cmd("VimwikiIndex")  -- Open Vimwiki
--- lua/custom/local_git_fun.lua
+++ lua/custom/local_git_fun.lua
@@ -2,22 +2,20 @@
 
 -- Define a function to push the current branch
 function _G.push_current_branch()
-    local branch_name = vim.fn.systemlist("git rev-parse --abbrev-ref HEAD")[1]
-    if not branch_name then
-        print("Could not determine the current branch.")
-        return
-    end
+  local branch_name = vim.fn.systemlist("git rev-parse --abbrev-ref HEAD")[1]
+  if not branch_name then
+    print("Could not determine the current branch.")
+    return
+  end
 
-    -- Construct and execute the git push command
-    local push_command = string.format('git push -u origin %s', branch_name)
-    local handle = io.popen(push_command)
-    local exit_status = handle:read("*a")
-    handle:close()
+  -- Construct and execute the git push command
+  local push_command = string.format("git push -u origin %s", branch_name)
+  local handle = io.popen(push_command)
+  local exit_status = handle:read("*a")
+  handle:close()
 
-    print(exit_status)
+  print(exit_status)
 end
-
-
 
 -- Change the LCD to the current Git buffer root
 function ChangeWorkingDirectoryToGitRoot()
@@ -33,5 +31,3 @@ function ChangeWorkingDirectoryToGitRoot()
     print("Not in a Git repository")
   end
 end
-
-
