b8abcf4e2f40ba163ac203411e8464e7a4c5da53
cajone <peat@blueyonder.co.uk>
2025-12-29 15:36:56 +0000

    Added new Git plugin configuration

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -5,19 +5,17 @@ local M = {
     keys = {
       { "<leader>gs",  ":Git<CR>",                                        desc = "Git Status" },
       { "<leader>gp",  ":Git push<CR>",                                   desc = "Git Push" },
-      -- CHANGED: Moved Blame to <leader>gm (m for blaMe) to free up 'b'
       { "<leader>gm",  ":Git blame<CR>",                                  desc = "Git Blame" },
       { "<leader>gls", ':G log --pretty=format:"%h - %an, %ar : %s"<CR>', desc = "Short Git Log" },
     },
   },
   { "tpope/vim-rhubarb" },
 
-  -- Telescope Git: The "Picker" tools (Moved from keys.lua to here)
+  -- Telescope Git: The "Picker" tools
   {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     keys = {
-      -- FIXED: 'b' is now strictly for Branches
       {
         "<leader>gb",
         function()
@@ -48,12 +46,58 @@ local M = {
     end,
   },
 
-  -- Diffview & Gitsigns (kept as before...)
-  { "sindrets/diffview.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+  -- Diffview: The "Review" tool
+  {
+    "sindrets/diffview.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>gd", "<cmd>DiffviewOpen<cr>",  desc = "Diffview Open" },
+      { "<leader>gq", "<cmd>DiffviewClose<cr>", desc = "Diffview Close" },
+    },
+  },
+
+  -- GitSigns: The "Gutter/Hunk" tool
   {
     "lewis6991/gitsigns.nvim",
     config = function()
-      require("gitsigns").setup()
+      require("gitsigns").setup({
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
+
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            opts.desc = opts.desc
+            vim.keymap.set(mode, l, r, opts)
+          end
+
+          -- Navigation between hunks
+          map("n", "]c", function()
+            if vim.wo.diff then
+              return "]c"
+            end
+            vim.schedule(function()
+              gs.next_hunk()
+            end)
+            return "<Ignore>"
+          end, { expr = true, desc = "Jump to next git hunk" })
+
+          map("n", "[c", function()
+            if vim.wo.diff then
+              return "[c"
+            end
+            vim.schedule(function()
+              gs.prev_hunk()
+            end)
+            return "<Ignore>"
+          end, { expr = true, desc = "Jump to prev git hunk" })
+
+          -- Inline Actions
+          map("n", "<leader>hp", gs.preview_hunk, { desc = "Preview hunk in floating window" })
+          map("n", "<leader>hs", gs.stage_hunk, { desc = "Stage current hunk" })
+          map("n", "<leader>hr", gs.reset_hunk, { desc = "Reset/Discard current hunk" })
+        end,
+      })
     end,
   },
 }
