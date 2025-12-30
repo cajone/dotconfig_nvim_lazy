de9ecf0c4f9536a5ab6d2b0765cbe54cb41d50df
cajone <peat@blueyonder.co.uk>
2025-12-30 10:15:21 +0000

    Tidied up plugin loader

--- lua/plugins/lazy_load.lua
+++ lua/plugins/lazy_load.lua
@@ -25,14 +25,9 @@ require("lazy").setup({
 
   change_detection = { notify = false }, -- Stop reporting auto changes
 
-  -- Plugins used straight out the box
-  --  { "instant-markdown/vim-instant-markdown" },     -- Instant Markdown
-  --  { "vimwiki/vimwiki" },                           -- Vimwiki
-  --  { "MeanderingProgrammer/render-markdown.nvim" }, -- ADDED/MOVED HERE: Ensure render-markdown is a top-level plugin
-  -- { import = "plugins.kiwi" },         --# vimwiki alternative
-  { import = "plugins.neowiki" },      --# vimwiki alternative
-  { "nvim-telescope/telescope.nvim" }, -- ADDED HERE: Ensure Telescope is loaded early as a top-level plugin
-  -- { import = "plugins.obsidian" },                 -- obsidian note taker
+  { "nvim-telescope/telescope.nvim" },   -- ADDED HERE: Ensure Telescope is loaded early as a top-level plugin
+
+  { import = "plugins.neowiki" },        --# vimwiki alternative
   { import = "plugins.treesitter" },
   --  { import = "plugins.arduino" },  -- Arduino front end
   --  { import = "plugins.lint" },      -- Linter(s)
@@ -46,14 +41,12 @@ require("lazy").setup({
   { import = "plugins.ui.lualine" },     -- Status Line
   -- { import = "plugins.ui.mini" },        -- Collections on notes, todo's
   { import = "plugins.ui.telescope" },   -- Fuzzy file finder and many other things
-  -- { import = "plugins.ui.todo" },        -- Todo notes etc
   { import = "plugins.ui.conform" },     -- Formatting, linting
-  --  { import = "plugins.ui.fzf-lua" }, -- Formatting, linting
 
   -- AI based plugins
   --  { import = "plugins.ai.mcphub" }, -- Configure AI prompt
   { import = "plugins.ai.copilot" }, -- Configure AI prompt
-  { import = "plugins.ai.avante" },  -- AI frontend
+  -- { import = "plugins.ai.avante" },  -- AI frontend
   -- { import = "plugins.ai.opencode" }, -- AI frontend
   -- { import = "plugins.ai.llm",        lazy = false }, -- ai litellm
 
